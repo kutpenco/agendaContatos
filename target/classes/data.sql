c2e0d643028827aa442909f4bba4d665e6071e74
Lucas Penco <lucas.penco@gmail.com>
2021-09-16 21:43:12 -0300

    Initial
    Initial Release

---
+++ target/classes/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO acesso VALUES (null,'Lucas Penco','Penco','lucas.penco@gmail.com',1);
+
+--INSERT INTO usuario VALUES (null,'lucas.penco@gmail.com','$2a$12$pdlJY7IGuULz44gDcwzOKeW0YhIGRwPG3aHUw5TxMxgUV60KEwp6y');
+
+--INSERT INTO perfil VALUES(null,'USUARIO');
+--INSERT INTO perfil VALUES(null,'ADM');
+
+--INSERT INTO usuario_perfis VALUES (1,1);

58dbd8849e05dd18408a77fb0304a028a5ee1e76
Lucas Penco <lucas.penco@gmail.com>
2021-09-23 21:44:20 -0300

    Email Bug Fix

--- src/main/java/br/com/madrugas/agenda/model/dto/EmailDtoInput.java
+++ src/main/java/br/com/madrugas/agenda/model/dto/EmailDtoInput.java
@@ -3,12 +3,12 @@ package br.com.madrugas.agenda.model.dto;
 public class EmailDtoInput {
 	
 	private String email;
-
-	public EmailDtoInput (String email) {
-		this.email = email;
-	}
-
+	
 	public String getEmail() {
 		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
